283b96a0eea46f36b90bb16ba6a03050abc1cc3c
SpeedyM28 <jamil.bitarm76@gmail.com>
2024-12-01 04:14:39 -0500

    Completed practice exercise

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer>
+            <p>This is the footer, idk what to put here</p>
+            <p>nvm i cheated and looked at what they gave</p>
+            <p>&copy; Event Planner Organization. All rights reserved.</p>
+        </footer>
+        );
+};
+
+export default Footer;
